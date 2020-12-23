5c220847c0cb7c32618c9c9643168d740ed6878b
Ewan Oomen <ewanoomen@gmail.com>
2020-12-23 15:46:51 +0100

    moved geetings

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
-    page += generator.generate_buzz()
     page += "Greetings from Ewan </br>"
+    page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
 
